55a6bd3ec8e22d48e4c7963b892d8d3d3ce74825
Riley Parsons <rparsons@scu.edu>
2015-03-20 19:31:12 -0700

    Implemented PFQueryTableViewController

---
+++ AllPostsTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  AllPostsTableViewController.h
+//  
+//
+//  Created by Riley Steele Parsons on 3/20/15.
+//
+//
+
+#import <ParseUI/ParseUI.h>
+#import <Parse/Parse.h>
+
+@interface AllPostsTableViewController : PFQueryTableViewController
+
+@end
