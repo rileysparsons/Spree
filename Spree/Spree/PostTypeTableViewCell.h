efb4513b8dd1598e33a3a388547b9980eae338e6
Riley Parsons <rparsons@scu.edu>
2015-05-11 00:11:55 -0700

    Added new UI to the cells on Category table view

--- Spree/Spree/PostTypeTableViewCell.h
+++ Spree/Spree/PostTypeTableViewCell.h
@@ -14,5 +14,6 @@
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *numberLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *detailImage;
+@property (weak, nonatomic) IBOutlet UIView *iconBackground;
 
 @end
