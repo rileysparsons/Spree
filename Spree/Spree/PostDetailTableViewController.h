0a3e2db762ec1d5ea7cec2fa840c36e0c09a4e41
Riley Parsons <rparsons@scu.edu>
2015-06-10 15:48:01 -0700

    Added preliminary structure for a dynamic tableview.

---
+++ Spree/Spree/PostDetailTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  PostDetailTableViewController.h
+//  
+//
+//  Created by Riley Steele Parsons on 6/9/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "SpreePost.h"
+
+@interface PostDetailTableViewController : UITableViewController
+
+@property NSArray *fields;
+
+@property SpreePost *post;
+
+@end
---
+++ Spree/Spree/PostDetailTableViewController.m
@@ -0,0 +1,132 @@
+//
+//  PostDetailTableViewController.m
+//  
+//
+//  Created by Riley Steele Parsons on 6/9/15.
+//
+//
+
+#import "PostDetailTableViewController.h"
+#import "PostTitleTableViewCell.h"
+
+@interface PostDetailTableViewController ()
+
+@end
+
+@implementation PostDetailTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 100.0f;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return [self cellForField:self.fields[indexPath.row]];
+}
+
+-(UITableViewCell *)cellForField:(NSString *)field {
+    if ([field isEqualToString:PF_POST_TITLE]){
+        static NSString *CellIdentifier = @"TitleCell";
+        PostTitleTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            NSArray *nibFiles = [[NSBundle mainBundle] loadNibNamed:@"PostTitleTableViewCell" owner:self options:nil];
+            for(id currentObject in nibFiles){
+                if ([currentObject isKindOfClass:[UITableViewCell class]]){
+                    cell = (PostTitleTableViewCell*)currentObject;
+                    break;
+                }
+            }
+        }
+        [cell setTitleforPost:self.post];
+        return cell;
+    } else if ([field isEqualToString:PF_POST_DESCRIPTION]){
+        static NSString *CellIdentifier = @"TitleCell";
+        PostTitleTableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            NSArray *nibFiles = [[NSBundle mainBundle] loadNibNamed:@"PostTitleTableViewCell" owner:self options:nil];
+            for(id currentObject in nibFiles){
+                if ([currentObject isKindOfClass:[UITableViewCell class]]){
+                    cell = (PostTitleTableViewCell*)currentObject;
+                    break;
+                }
+            }
+        }
+        [cell setTitleforPost:self.post];
+        return cell;
+    }
+
+    return 0;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
