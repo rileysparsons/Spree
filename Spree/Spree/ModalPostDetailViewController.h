f097d0b5dc0fb637d00adef4abb2cb44a9042972
Riley Parsons <rparsons@scu.edu>
2015-09-04 16:16:49 -0700

    Links now open the appropriate post and can be dismissed.

---
+++ Spree/Spree/ModalPostDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModalPostDetailViewController.h
+//  Spree
+//
+//  Created by Riley Steele Parsons on 9/4/15.
+//  Copyright (c) 2015 Riley Steele Parsons. All rights reserved.
+//
+
+#import "PostDetailTableViewController.h"
+
+@interface ModalPostDetailViewController : PostDetailTableViewController
+
+@end
