3ce1d5d70f19b2d87310d1305b569813a1d8d51e
Riley Parsons <rparsons@scu.edu>
2015-12-16 15:16:54 -0800

    Destroyed current onboarding structure.

--- Spree-iOS/Spree/RTWalkthrough/BaseOnboardingViewController.m
+++ Spree-iOS/Spree/RTWalkthrough/BaseOnboardingViewController.m
@@ -10,8 +10,6 @@
 #import "RTWalkthroughPageViewController.h"
 #import "RTWalkthroughViewController.h"
 #import "FinalOnboardingViewController.h"
-#import "LoginCampusTableViewController.h"
-#import "LoginAuthorizationViewController.h"
 #import "PostTableViewController.h"
 #import "SpreeUtility.h"
 #import <ParseFacebookUtilsV4/PFFacebookUtils.h>
@@ -19,10 +17,6 @@
 
 @interface BaseOnboardingViewController ()
 
-@property LoginAuthorizationViewController *authorizationViewController;
-@property LoginPasswordViewController *passwordViewController;
-@property LoginEmailViewController *emailViewController;
-@property LoginCampusTableViewController *campusViewController;
 @property PFUser *user;
 @property NSMutableArray *loginWorkflowViewControllers;
 
@@ -35,25 +29,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self startOnboarding];
-    [self prepareLoginWorkflow];
     self.user = [PFUser user];
     
     // Do any additional setup after loading the view.
 }
 
--(void)prepareLoginWorkflow{
-    UIStoryboard *stb = [UIStoryboard storyboardWithName:@"Walkthrough" bundle:nil];
-    self.authorizationViewController =  [stb instantiateViewControllerWithIdentifier:NSStringFromClass([LoginAuthorizationViewController class])];
-    [self.authorizationViewController setDelegate:self];
-    self.passwordViewController =  [stb instantiateViewControllerWithIdentifier:NSStringFromClass([LoginPasswordViewController class])];
-    [self.passwordViewController setDelegate:self];
-    self.emailViewController = [stb instantiateViewControllerWithIdentifier:@"EmailInput"];
-    [self.emailViewController setDelegate:self];
-    self.campusViewController =
-    [stb instantiateViewControllerWithIdentifier:NSStringFromClass([LoginCampusTableViewController class])];
-    self.campusViewController.user = [PFUser user];
-    [self.campusViewController setDelegate:self];
-}
 
 -(void)startOnboarding{
     UIStoryboard *stb = [UIStoryboard storyboardWithName:@"Walkthrough" bundle:nil];
@@ -78,103 +58,13 @@
 -(void)walkthroughControllerDidClose:(RTWalkthroughViewController *)controller{
     NSLog(@"called");
     [self.navigationController dismissViewControllerAnimated:NO completion:nil];
-    [self.navigationController pushViewController:self.campusViewController animated:YES];
+//    [self.navigationController pushViewController:self.campusViewController animated:YES];
+    // This is where the login process begins.
 }
 
--(BOOL)logInViewController:(LoginEmailViewController *)logInController shouldBeginLogInWithEmail:(NSString *)email{
-    if (email && email.length != 0) {
-        return YES;
-    }
-    return NO; // Interrupt login process
-}
-
--(void)logInViewController:(LoginEmailViewController *)logInController didCheckEmail:(NSString *)email userExists:(BOOL)exists{
-    if (exists){
-        self.passwordViewController.userIsNew = NO;
-    } else {
-        self.passwordViewController.userIsNew = YES;
-    }
-    NSLog(@"%@", self.navigationController.viewControllers);
-    self.passwordViewController.user = logInController.user;
-    NSLog(@"%@", logInController.user);
-    [self.navigationController pushViewController:self.passwordViewController animated:YES];
-}
 
 #pragma mark - LoginAuthorizationViewControllerDelegate Methods
-
-- (void)logInAuthorizationViewController:(LoginAuthorizationViewController *)logInController didAuthorizeFacebookForUser:(PFUser *)user{
-
-
-    
-    [self closeOnboarding];
-}
-
-- (void)logInAuthorizationViewController:(LoginAuthorizationViewController *)logInController didFailToAuthorizeFacebookForUser:(PFUser *)user{
-    [[[UIAlertView alloc] initWithTitle:@"Facebook Authorization Failed"
-                                message:@"Something went wrong. Please try again."
-                               delegate:nil
-                      cancelButtonTitle:@"OK"
-                      otherButtonTitles:nil] show];
-}
-
-- (void)logInAuthorizationViewControllerWentBackwards:(LoginAuthorizationViewController *)authorizationController{
-    [self.navigationController popViewControllerAnimated:YES];
-}
-
-- (void)logInAuthorizationViewController:(LoginAuthorizationViewController *)authorizationController userDidOptOut:(PFUser *)user{
-    [self closeOnboarding];
-}
-
-#pragma mark - LoginCampusTableViewController Method
-
--(void)loginCampusTableViewController:(LoginCampusTableViewController *)loginCampusTableViewController didSelectCampus:(PFObject *)campus{
-    NSString *fullDomain = [NSString stringWithFormat:@"@%@.edu", campus[@"networkCode"]];
-    self.emailViewController.domain = fullDomain;
-    self.emailViewController.user = loginCampusTableViewController.user;
-    [self.emailViewController.user setObject:campus[@"networkCode"] forKey:@"network"];
-    NSLog(@"%@", loginCampusTableViewController.user);
-    [self.navigationController pushViewController:self.emailViewController animated:YES];
-}
-
-#pragma mark - LoginPasswordViewControllerDelegate
-
--(BOOL)logInViewController:(LoginPasswordViewController *)logInController shouldBeginLogInWithPassword:(NSString *)password{
-    NSLog(@"%@", password);
-    if (password && password.length != 0) {
-        return YES;
-    }
-    
-    return NO; // Interrupt login process
-}
-
-
--(void)logInViewController:(LoginPasswordViewController *)logInController didLogInUser:(PFUser *)user{
-    
-#warning REMOVE BEFORE SUBMISSION
-    Branch *branch = [Branch getInstance];
-    [branch setIdentity:[PFUser currentUser].objectId];
-
-    if (![PFFacebookUtils isLinkedWithUser:user]){
-        self.authorizationViewController.user = user;
-        [self.navigationController pushViewController:self.authorizationViewController animated:YES];
-    } else if (![SpreeUtility userHasValidFacebookData:[PFUser currentUser]]){
-        self.authorizationViewController.user = user;
-        [self.navigationController pushViewController:self.authorizationViewController animated:YES];
-    } else {
-        [self closeOnboarding];
-    }
-}
-
-- (void)logInViewController:(LoginPasswordViewController *)logInController
-    didFailToLogInWithError:(NSError *)error{
-    
-}
-
--(void)logInViewControllerWentBackwards:(LoginPasswordViewController *)logInController{
-    [self.navigationController popViewControllerAnimated:YES];
-}
-
-
+/*
 -(void)signupViewController:(LoginPasswordViewController *)signupController didSignUpUser:(PFUser *)user{
     #warning REMOVE BEFORE SUBMISSION
     Branch *branch = [Branch getInstance];
@@ -184,24 +74,8 @@
     self.authorizationViewController.user = user;
     [self.navigationController pushViewController:self.authorizationViewController animated:YES];
 }
-
--(void)signupViewController:(LoginPasswordViewController *)signupController didFailToSignUpWithError:(NSError *)error{
-    [[[UIAlertView alloc] initWithTitle:@"Sign Up Failed"
-                                message:@"Something went wrong. Please try again."
-                               delegate:nil
-                      cancelButtonTitle:@"OK"
-                      otherButtonTitles:nil] show];
-}
-
-- (BOOL)signupViewController:(LoginPasswordViewController *)signupController shouldBeginSignInWithPassword:(NSString *)password{
-    NSLog(@"%@", password);
-    if (password && password.length != 0){
-        return YES;
-    }
-    return NO;
-}
-
-
+*/
+ 
 //// Sent to the delegate when a PFUser is signed up.
 //- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
 //    UIApplication* sharedApplication = [UIApplication sharedApplication];
